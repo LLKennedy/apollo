fa5d603ca0174a781abe4d4322673f6e4d6e297f
Luke Kennedy <luke.l.kennedy@gmail.com>
2019-12-21 21:39:36 +1100

    Implement panic-safe virtual patch cable

---
+++ examples/twosine/main.go
@@ -0,0 +1,5 @@
+package main
+
+func main() {
+	// Play two sine waves, one in each ear.
+}
--- patches/virtualcable/patch.go
+++ patches/virtualcable/patch.go
@@ -1,1 +1,35 @@
 package virtualcable
+
+// Patch is a virtual patch cable
+type Patch struct {
+	closer chan bool
+}
+
+// New creates a new virtual patch cable
+func New(from <-chan byte, to chan<- byte) *Patch {
+	p := new(Patch)
+	go p.connect(from, to)
+	return p
+}
+
+func (p *Patch) connect(from <-chan byte, to chan<- byte) {
+	defer recover() // Ignore all panics
+	for {
+		select {
+		case to <- <-from:
+			continue
+		case shouldClose := <-p.closer:
+			if shouldClose {
+				return
+			}
+		}
+
+	}
+}
+
+// Close ends
+func (p *Patch) Close() {
+	if p != nil && p.closer != nil {
+		p.closer <- true
+	}
+}
